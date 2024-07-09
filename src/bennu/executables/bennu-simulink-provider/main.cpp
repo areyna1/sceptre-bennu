f8ea3942fe19125c31f76c29a4cbe23d7747a376
aareyna <aareyna@sandia.gov>
2024-07-08 18:10:33 -0600

    adding changes to make file for publish rate

--- src/bennu/executables/bennu-simulink-provider/main.cpp
+++ src/bennu/executables/bennu-simulink-provider/main.cpp
@@ -24,32 +24,32 @@ using namespace bennu::distributed;
 /* *****************************************************************
  * Global Constants (VALUES MUST MATCH CONSTANTS IN SIMULINK SOLVER)
  ***************************************************************** */
-/* shared memory for PublishPoints */
-const unsigned int NUM_PUBLISH_POINTS_SHM_KEY {10610};
-const unsigned int PUBLISH_POINTS_SHM_KEY     {10613};
+ /* shared memory for PublishPoints */
+const unsigned int NUM_PUBLISH_POINTS_SHM_KEY{ 10610 };
+const unsigned int PUBLISH_POINTS_SHM_KEY{ 10613 };
 
 /* fifo queue for UpdatePoints messages */
 static const char* UPDATES_FIFO = "/tmp/updates_fifo";
 
 /* provider <-> solver sync */
-const unsigned int MAX_MSG_LEN {256};
-const char* PUBLISH_SEM  {"publish_sem"};
-const char* UPDATES_SEM  {"updates_sem"};
+const unsigned int MAX_MSG_LEN{ 256 };
+const char* PUBLISH_SEM{ "publish_sem" };
+const char* UPDATES_SEM{ "updates_sem" };
 
 /* ********************************
  * Global Internal Helper Variables
  ******************************** */
-/* used for setting the printed output precision of doubles / floats */
-const unsigned int PRECISION {10};
+ /* used for setting the printed output precision of doubles / floats */
+const unsigned int PRECISION{ 10 };
 
 static const unsigned int EXIT_ERROR = 1;
 
 struct Dto
 {
-    std::string tag {""};
-    std::string type {""};
-    std::string data {""};
-    std::string field {"processModelIO"}; /* field name default when not provided */
+    std::string tag{ "" };
+    std::string type{ "" };
+    std::string data{ "" };
+    std::string field{ "processModelIO" }; /* field name default when not provided */
 
     Dto(std::string dto)
     {
@@ -85,10 +85,10 @@ struct Dto
 
     std::string getDataString()
     {
-        std::stringstream strVal{data};
+        std::stringstream strVal{ data };
         strVal.precision(PRECISION);
 
-        std::stringstream ret {""};
+        std::stringstream ret{ "" };
 
         if ("DOUBLE" == type)
         {
@@ -118,22 +118,35 @@ struct Dto
 };
 
 
+    constexpr auto durationToDuration(const float time_s)
+{
+    using namespace std::chrono;
+    using fsec = duration<float>;
+    return round<nanoseconds>(fsec{time_s});
+}
+
+
 class BennuSimulinkProvider : public Provider
 {
 public:
-    BennuSimulinkProvider(const Endpoint& serverEndpoint, const Endpoint& publishEndpoint, bool debug) :
+    // creating publishRate_setting class variable which is set to parameter value publishRate
+    double publishRate_setting;
+    BennuSimulinkProvider(const Endpoint& serverEndpoint, const Endpoint& publishEndpoint, bool debug, double publishRate) :
         Provider(serverEndpoint, publishEndpoint),
         mLock(),
         mDebug(debug)
     {
+        publishRate_setting = publishRate;
+
         mPublishSemaphore = sem_open(PUBLISH_SEM, O_CREAT, 0644, 0);
-        if(SEM_FAILED == mPublishSemaphore)
+
+        if (SEM_FAILED == mPublishSemaphore)
         {
             std::cout << "Fatal: Unable to attach to publish semaphore" << std::endl;
             exit(EXIT_ERROR);
         }
         mUpdatesSemaphore = sem_open(UPDATES_SEM, O_CREAT, 0644, 0);
-        if(SEM_FAILED == mUpdatesSemaphore)
+        if (SEM_FAILED == mUpdatesSemaphore)
         {
             std::cout << "Fatal: Unable to attach to updates semaphore" << std::endl;
             exit(EXIT_ERROR);
@@ -144,7 +157,7 @@ public:
             /* Get number of PublishPoints from shared memory */
             int numPublishPointsShmId = shmget(NUM_PUBLISH_POINTS_SHM_KEY, sizeof(unsigned int), IPC_CREAT | 0666);
             char* numPublishPointsShmAddress = (char*)shmat(numPublishPointsShmId, NULL, 0);
-            std::stringstream numPublishPointsStr {numPublishPointsShmAddress};
+            std::stringstream numPublishPointsStr{ numPublishPointsShmAddress };
             numPublishPointsStr >> mNumPublishPoints;
             shmdt(numPublishPointsShmAddress);
             std::cout << "Info: Read " << mNumPublishPoints << " PublishPoints" << std::endl;
@@ -176,7 +189,7 @@ public:
         char* shmPtr = mPublishPointsShmAddress;
         for (int i = 0; i < mNumPublishPoints; i++)
         {
-            Dto dto{shmPtr};
+            Dto dto{ shmPtr };
             std::string tag = dto.tag + "." + dto.field;
             if (dto.field == "processModelIO") {
                 tag = dto.tag;
@@ -194,11 +207,11 @@ public:
         if (mDebug) { std::cout << "BennuSimulinkProvider::read ---- received read for tag: " << tag << std::endl; }
         std::scoped_lock<std::shared_mutex> lock(mLock);
         sem_wait(mPublishSemaphore);
-        std::string result{""};
+        std::string result{ "" };
         char* shmPtr = mPublishPointsShmAddress;
         for (int i = 0; i < mNumPublishPoints; i++)
         {
-            Dto dto{shmPtr};
+            Dto dto{ shmPtr };
             std::string name = dto.tag + "." + dto.field;
             if (dto.field == "processModelIO") {
                 name = dto.tag;
@@ -224,21 +237,21 @@ public:
     }
 
     // Must return "ACK=<success message>" or "ERR=<error message>"
-    std::string write(const std::unordered_map<std::string,std::string>& tags)
+    std::string write(const std::unordered_map<std::string, std::string>& tags)
     {
         std::string result;
 
         sem_wait(mUpdatesSemaphore);
-        for (auto &it : tags)
+        for (auto& it : tags)
         {
-            std::string tag{it.first};
-            std::string value{it.second};
+            std::string tag{ it.first };
+            std::string value{ it.second };
 
             // recieve write and format newDto
             if (mDebug) { std::cout << "BennuSimulinkProvider::write ---- received write for tag: " << tag << " -- " << value << std::endl; }
             std::scoped_lock<std::shared_mutex> lock(mLock);
             std::string dataStr, dataType;
-            if(value == "true" || value == "false")
+            if (value == "true" || value == "false")
             {
                 dataStr = value == "true" ? "1" : "0";
                 dataType = "BOOLEAN";
@@ -261,7 +274,8 @@ public:
                 std::cout << "\tMsg: " << newDto << std::endl;
                 result += "ERR=Problem writing tag in simulink provider";
                 break;
-            } else if (bytes == 0) {
+            }
+            else if (bytes == 0) {
                 std::cout << "Error: No bytes were written to FIFO" << std::endl;
                 std::cout << "\tMsg: " << newDto << std::endl;
                 result += "ERR=Nothing was written to the simulink provider";
@@ -284,7 +298,9 @@ public:
         {
             publishData();
             std::cout << std::flush;
-            std::this_thread::sleep_for(std::chrono::seconds(1));
+            // std::this_thread::sleep_for(std::chrono::seconds(1));
+            // changing sleep for when update of publishPoint made to duration specified by publishRate_setting
+            std::this_thread::sleep_for(durationToDuration(this->publishRate_setting));
         }
     }
 
@@ -297,7 +313,7 @@ public:
         char* shmPtr = mPublishPointsShmAddress;
         for (int i = 0; i < mNumPublishPoints; i++)
         {
-            Dto dto{shmPtr};
+            Dto dto{ shmPtr };
             std::string tag = dto.tag + "." + dto.field;
             if (dto.field == "processModelIO") {
                 tag = dto.tag;
@@ -326,13 +342,14 @@ private:
 int main(int argc, char** argv)
 {
     std::cout.precision(PRECISION);
-    bool debug{false};
+    bool debug{ false };
     po::options_description desc("Simulink Provider");
     desc.add_options()
-        ("help",  "show this help menu")
+        ("help", "show this help menu")
         ("debug", po::bool_switch(&debug), "print debugging information")
         ("server-endpoint", po::value<std::string>()->default_value("tcp://127.0.0.1:5555"), "server listening endpoint")
-        ("publish-endpoint", po::value<std::string>()->default_value("udp://239.0.0.1:40000"), "publishing endpoint");
+        ("publish-endpoint", po::value<std::string>()->default_value("udp://239.0.0.1:40000"), "publishing endpoint")
+        ("publish-rate", po::value<double>()->default_value(0.1), "rate at which updates published from provider to simulation");
 
     po::variables_map vm;
     po::store(po::parse_command_line(argc, argv, desc), vm);
@@ -347,7 +364,8 @@ int main(int argc, char** argv)
     Endpoint sEndpoint, pEndpoint;
     sEndpoint.str = vm["server-endpoint"].as<std::string>();
     pEndpoint.str = vm["publish-endpoint"].as<std::string>();
-    BennuSimulinkProvider bsp(sEndpoint, pEndpoint, debug);
+    double publishRate = vm["publish-rate"].as<double>();
+    BennuSimulinkProvider bsp(sEndpoint, pEndpoint, debug, publishRate);
     bsp.run();
     return 0;
 }
