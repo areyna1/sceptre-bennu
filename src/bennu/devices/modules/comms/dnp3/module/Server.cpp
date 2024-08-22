9b526518223437227ede3571a7346f880d9ac84e
Alexander Arthur Reyna <aareyna@sandia.gov>
2024-08-22 17:19:50 -0600

    Update Server.cpp

--- src/bennu/devices/modules/comms/dnp3/module/Server.cpp
+++ src/bennu/devices/modules/comms/dnp3/module/Server.cpp
@@ -15,9 +15,8 @@ namespace bennu {
 namespace comms {
 namespace dnp3 {
 
-Server::Server(std::shared_ptr<field_device::DataManager> dm, std::chrono::seconds pollRate) :
-    bennu::utility::DirectLoggable("dnp3-server"),
-    mPollRate(pollRate)
+Server::Server(std::shared_ptr<field_device::DataManager> dm) :
+    bennu::utility::DirectLoggable("dnp3-server")
 {
     // Initialize outstation stack
     mManager.reset(new opendnp3::DNP3Manager(std::thread::hardware_concurrency(), opendnp3::ConsoleLogger::Create()));
@@ -161,7 +160,7 @@ void Server::update()
                 mOutstation->Apply(builder.Build());
             }
         }
-        std::this_thread::sleep_for(durationToDuration(this->pollRate));
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 }
 
